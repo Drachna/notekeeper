4723f90d87db581df8d98b1a70c02dbfba733764
dev-rachna <rachna.devasthali@yahoo.com>
2021-01-20 21:43:21 +0530

    clean ups and additions

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -8,24 +8,6 @@ router.get('/api/logout',authController.logout_get)
 router.get('/api/getUser',requireAuthentication,authController.auth_get)
 
 
-// router.get('/getUser',(req,res)=>{
-//   console.log(req.user);
-//   let data;
-//   if (req.user){
-//    data={
-//     status:"Authenticated",
-//     user:req.user
-//   }
-// }
-// else{
-//    data={
-//     status:"LOGGED_OUT",
-//     user:req.user
-// }
-// }
-//   return res.send(data)
-
-// })
 
 
 
--- server/routes/notesRoute.js
+++ server/routes/notesRoute.js
@@ -2,9 +2,9 @@ const router=require('express').Router()
 const requireAuthentication =require('../middlewares/authMiddleware')
 const notesController=require('../controllers/notesController')
 
-router.post('/api/notes/addNote',notesController.addNotes)
+router.post('/api/notes/addNote',requireAuthentication,notesController.addNotes)
 router.post('/api/notes/edit',requireAuthentication,notesController.updateNotes)
-router.get('/api/notes/get_note',notesController.findNotes)
+router.get('/api/notes/get_note',requireAuthentication,notesController.findNotes)
 router.delete('/api/note/deleteNote/:id',requireAuthentication,notesController.deleteNote);
 
 module.exports=router
